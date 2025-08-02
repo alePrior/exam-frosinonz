c7290f80e47f78bbbf35b45582900e76b6a74a8b
Alessandro <priori.alessandro94@hotmail.it>
2025-08-02 17:31:02 +0200

    aggiunta api call chart.getTopArtisis

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+import config from './config.json';
+const api_chart_gettopartists = `https://ws.audioscrobbler.com/2.0/?method=chart.getTopArtists&api_key=${config.API_KEY}&format=json`;
+
 function setup() {
   createCanvas(400, 400);
   background(220);
